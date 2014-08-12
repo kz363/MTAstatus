be2e2084c3f9872feee25783b70f219fbf789404
Kevin <kz363@nyu.edu>
2014-08-12 04:43:20 -0400

    Fix cron job code in app initialize

--- config/application.rb
+++ config/application.rb
@@ -5,10 +5,10 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module MTAstatus
-  class Application < Rails::Application
-    config.after_initialize do
-      update_cache
-      system('bundle exec whenever --update-crontab')
-    end
-  end
+	class Application < Rails::Application
+		config.after_initialize do
+			update_cache
+			system('bundle exec whenever --update-crontab')
+		end
+	end
 end
