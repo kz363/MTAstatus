dd9fbc005b1df56c81e95ec5d4791ab257de286d
Kevin <kz363@nyu.edu>
2014-08-12 00:12:08 -0400

    Bind AJAX request to form submission to get the table to append to the page. Adjust CSS of page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,8 @@ class PagesController < ApplicationController
 	end
 
 	def status
-
+		service = params[:service].to_sym
+		service_table = Rails.cache.read('statuses')[service]
+		render json: service_table.to_json
 	end
 end
--- config/initializers/status_setup.rb
+++ config/initializers/status_setup.rb
@@ -6,17 +6,23 @@ xml = Nokogiri::XML(open('serviceStatus.txt')) do |config|
 			end
 
 timestamp = xml.xpath('//timestamp').text
-status_header = "<h4>Service Status</h4><h6>as of #{timestamp}</h6><br>"
+status_header = "<h4>Service Status as of #{timestamp}</h4>"
 
 # Each key in the hash will be one of the 5 services from the MTA
 # The values are an HTML string of a table to append to the page
-service_statuses = Hash.new("#{status_header}<table class='service_table'>")
+service_statuses = Hash.new("<div class='service_table'>#{status_header}<table>")
 
+# Append line information as a row in the table
 xml.xpath('//line').each do |node| 
 	service = node.parent.name.downcase.to_sym
 	line_name = node.children[0].text
 	line_status = node.children[1].text
 	service_statuses[service] += "<tr><td>#{line_name}</td><td>#{line_status}</td></tr>" 
+end
+
+# Close HTML tags
+service_statuses.each do |k,v|
+	service_statuses[k] = v + "</table></div>"
 end
 
 Rails.cache.write('statuses', service_statuses)
